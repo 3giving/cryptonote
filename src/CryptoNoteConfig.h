eaacc2416931272a52f6d79659bd1160245c57c2
3giving <paul@goliathpublishing.com>
2016-04-09 11:46:19 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,9 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
-};
+  //"3giving.com:8080",
+  "160.153.16.18"};
 
 struct CheckpointData {
   uint32_t height;
